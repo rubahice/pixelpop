0860cd4302b52c83c0844ee9aeae0e0e30132267
xeizer <keiser.form@gmail.com>
2024-02-26 12:52:02 +0700

    penambahan selection brands

--- resources/views/livewire/frontend/product/index.blade.php
+++ resources/views/livewire/frontend/product/index.blade.php
@@ -2,12 +2,15 @@
     <div class="row">
         <div class="col-md-3">
             <div class="card">
-                <div class="card-header"><h4>Brands</h4></div>
+                <div class="card-header">
+                    <h4>Brand</h4>
+                </div>
                 <div class="card-body">
                     @foreach ($category->brands as $brandItem)
-                    <label class="d-block">
-                        <input type="checkbox" wire:model="brandInputs" value="{{ $brandItem->name }}" /> {{ $brandItem->name }}
-                    </label>
+                        <label class="d-block">
+                            <input type="checkbox" wire:model.live="brandInputs" value="{{ $brandItem->name }}" />
+                            {{ $brandItem->name }}
+                        </label>
                     @endforeach
                 </div>
             </div>
@@ -20,38 +23,43 @@
                         <div class="product-card">
                             <div class="product-card-img">
                                 @if ($productItem->quantity > 0)
-                                <label class="stock bg-success">In Stock</label>
+                                    <label class="stock bg-success">In Stock</label>
                                 @else
-                                <label class="stock bg-danger">Out of Stock</label>
+                                    <label class="stock bg-danger">Out of Stock</label>
                                 @endif
 
                                 @if ($productItem->productImages->count() > 0)
-                                <a href="{{ url('/collections/'.$productItem->category->slug.'/'.$productItem->slug) }}">
-                                    <img src="{{ asset($productItem->productImages[0]->image) }}" alt="{{ $productItem->name }}">
-                                </a>
+                                    <a
+                                        href="{{ url('/collections/' . $productItem->category->slug . '/' . $productItem->slug) }}">
+                                        <img src="{{ asset($productItem->productImages[0]->image) }}"
+                                            alt="{{ $productItem->name }}">
+                                    </a>
                                 @endif
                             </div>
                             <div class="product-card-body">
                                 <p class="product-brand">{{ $productItem->brand }}</p>
                                 <h5 class="product-name">
-                                <a href="{{ url('/collections/'.$productItem->category->slug.'/'.$productItem->slug) }}">
+                                    <a
+                                        href="{{ url('/collections/' . $productItem->category->slug . '/' . $productItem->slug) }}">
                                         {{ $productItem->name }}
-                                </a>
+                                    </a>
                                 </h5>
                                 <div>
-                                    <span class="selling-price">Rp.{{ number_format($productItem->selling_price) }}</span>
-                                    <span class="original-price">Rp.{{ number_format($productItem->original_price) }}</span>
+                                    <span
+                                        class="selling-price">Rp.{{ number_format($productItem->selling_price) }}</span>
+                                    <span
+                                        class="original-price">Rp.{{ number_format($productItem->original_price) }}</span>
                                 </div>
 
                             </div>
                         </div>
                     </div>
                 @empty
-                <div class="col-md-12">
-                    <div class="p-2">
-                        <h4>No Products Available {{ $category->name }}</h4>
+                    <div class="col-md-12">
+                        <div class="p-2">
+                            <h4>No Products Available {{ $category->name }}</h4>
+                        </div>
                     </div>
-                </div>
                 @endforelse
             </div>
 
